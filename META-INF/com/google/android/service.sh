f8f80c098cab6d3c213b33d85b9ae6462c3b02f0
It's me <104527222+its-me-cyber@users.noreply.github.com>
2023-03-29 10:12:27 +0000

    Delete service.sh

--- META-INF/com/google/android/service.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-
-# Set Wi-Fi country code to JP on boot
-setprop persist.wifi.country JP
-
-# Run the command to force Wi-Fi country code to JP
-cmd wifi force-country-code enabled JP
